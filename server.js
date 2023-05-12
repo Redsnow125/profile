ee753cf618b61a762d52dfa9d50e0b1fbea5be41
Redsnow125 <128419959+Redsnow125@users.noreply.github.com>
2023-05-12 09:53:37 -0600

    Server Changes

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('public')) //The string in static() needs to be whatever your frontend folder is named.
+app.use(express.static('public'))
 
 app.get('/', (req,res) => {
-  res.status(200).sendFile(path.join(__dirname, './public/index.html'))
+  res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 app.listen(4000, console.log(`App running on 4000`))
